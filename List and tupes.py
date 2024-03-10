d7dbdfb7a117357b2889bfaa69768870f55acad8
cengi <cengizjan1457@gmail..com>
2024-03-10 22:33:05 +0300

    new

---
+++ List and tupes.py
@@ -0,0 +1,37 @@
+##PYTHON LISTS
+
+# l1= [1, 2.5,'Python', True, ['abc', 'xyz'], (10,20,30)]
+# print(len(l1))
+# l2=[] #empty list
+# l3 = list() #empty list
+# print(l1[0])
+# print(x := l1[-1])
+# l4 = list('abcd')
+# print(l4)
+# l4[0]='X'
+# l4.append(100)
+# print(l4)
+# l1=[3,4]
+# print(l1, id(l1))
+# l1=l1+[5,6]
+# print(l1, id(l1))
+# l1 += [7,8]
+# print(l1, id(l1))
+# l1.extend([11,12])
+# print(l1, id(l1))
+# #append vs extend
+# l1.append(['a','b'])
+# print(l1)
+# l1.extend(['x','y'])
+# print(l1)
+# l1.append(20)
+# l1.extend([20])
+# print(l1)
+# l2=list('abc')
+# l3=l2*3
+# print(l3)
+####### list slicing
+# numbers= [1,2,3,4,5]
+# nums = numbers[1:4]
+# print(f'{nums}')
+# print(f'{numbers}')
