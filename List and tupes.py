b4ea13369644f0a782aa7c07580adc49f3e43d11
cengi <cengizjan1457@gmail..com>
2024-03-10 23:33:11 +0300

    new

--- List and tupes.py
+++ List and tupes.py
@@ -35,3 +35,35 @@
 # nums = numbers[1:4]
 # print(f'{nums}')
 # print(f'{numbers}')
+######LIST COPY GOTCHAS
+# l1= [1,2,3]
+# l2=l1
+# l2[0]='XX'
+# l2.append(10)
+# print(f'l2:{l2}')
+# print(f'l1:{l1}')
+# print(id(l1),id(l2))
+# l1.remove(2)
+# print(f'l2:{l2}')
+# ## to create copy use copy method
+# l3=l1.copy()
+# l3.append('abc')
+# print(f'l1 {l1}')
+# print(f'l3 {l3}')
+# print(id(l1),id(l3))
+#2. lesson
+# nums = [1,2,3,4,5,6,7,0,1,2]
+# #This is wrong
+# # for n in nums:
+# #     if n <5:
+# #         nums.remove(n)
+# # print(nums)
+# #This true
+# new_list =list()
+# for n in nums:
+#     if n >=5:
+#         new_list.append(n)
+# print(new_list)
+#
+# my_list = [n for n in nums if n>=5]
+# print(my_list)
