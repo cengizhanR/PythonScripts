9c4fa5f23fd01cf33533c034158331589f47bf5c
cengi <cengizjan1457@gmail..com>
2024-03-14 22:23:19 +0300

    new

--- sets.py
+++ sets.py
@@ -60,4 +60,52 @@
 # s4=s1.copy()
 # s4.add('z')
 # print(f's4: {s4}, s1:{s1}')
-
+#SET METHODS PART2
+# set1={1,3,5,9}
+# set2={5,7,9}
+# #1. set.intersection()
+# set3=set1.intersection(set2)
+# print(set3)
+# set3=set1 & set2
+# print(set3)
+# #2. set.difference()
+# set4 = set1.difference(set2)
+# print(f'set4: {set4}')
+# set4= set1-set2
+# print(f'set4: {set4}')
+# set4=set1.difference([1,2,3,4,5])
+# print(f'set4: {set4}')
+# #3. set.symmetric_difference()
+# set5=set1.symmetric_difference(set2)
+# print(f'set5: {set5}')
+# set5=set1 ^ set2
+# print(f'set5: {set5}')
+# #4 set.union()
+# set6=set1.union(set2)
+# print(f'set6: {set6}')
+# set6=set1| set2
+# print(f'set6: {set6}')
+# #5 set.isdisjoint()
+# s1={1,3,5}
+# s2={5,6,7}
+# print(s1.isdisjoint(s2))
+# s3={8,9}
+# print(s1.isdisjoint(s3))
+# #<,<=,>,>=
+# print({1,3}<{1,2,3,4})
+#PYTHON FROZENSETS
+fs1=frozenset({1,2,3,'a','b'})
+print(fs1,type(fs1))
+s1='Python is cool!!'
+fs2=frozenset(s1)
+print(fs2)
+fs3=frozenset()
+fs1=frozenset([1,2,3,4])
+fs2=frozenset([3,4,5,6])
+fs3=fs1.intersection(fs2)
+print(fs3)
+s1={4,10,20}
+result1=s1.intersection(fs1)
+result2=fs1-s1
+print(f'result1 type: {type(result1)}') #set
+print(f'result2 type: {type(result2)}') #frozenset
