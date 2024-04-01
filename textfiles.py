0a1d1e508baf93a76736cdaa0698e2270cb8ed21
cengi <cengizjan1457@gmail..com>
2024-04-01 22:29:39 +0300

    new

--- textfiles.py
+++ textfiles.py
@@ -84,3 +84,144 @@ with open('devices.txt') as f:
 
     for device in devices:
         print(f'pinging {device[1]}')
+
+import csv
+# with open('airtravel.csv', 'r') as f:
+#     reader = csv.reader(f)
+#     next(reader) # it wil skipt he first line
+#     year_1958 = dict()
+#     for row in  reader:
+#         year_1958[row[0]]= row[1]
+#
+#     print(year_1958)
+#     max_1958 = max(year_1958.values())
+#     print(max_1958)
+#     for k, v in year_1958.items():
+#         if max_1958 == v:
+#             print(f'Busiest Month in 1958: {k}, Flights:{v.strip()}')
+
+# with open('people.csv', 'a') as csvfile:
+#     writer = csv.writer(csvfile)
+#     csvdata = (5, 'Anne', 'Amsterdam')
+#     writer.writerow(csvdata)
+
+# with open('numbers.csv','w',newline='') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(['x','x**2','x**3','x**4'])
+#     for x in range(1,101):
+#         writer.writerow([x,x**2,x**3,x**4])
+# with open('passwd.csv', 'r') as f:
+#     reader = csv.reader(f, delimiter=':', lineterminator='\n')
+#     for row in reader:
+#         print(row)
+# csv.register_dialect('hashes', delimiter='#', quoting=csv.QUOTE_NONE, lineterminator='\n')
+# with open('items.csv','r') as csvfile:
+#     reader= csv.reader(csvfile, dialect='hashes')
+#     for row in reader:
+#         print(row)
+#
+# with open('items.csv','a') as csvfile:
+#     writer = csv.writer(csvfile, dialect='hashes')
+#     writer.writerow(('anan', 111,3.3))
+# with open ('devices.txt', 'r') as f:
+#      content= f.read().splitlines()
+#      dedi=list()
+#      # print(content)
+#      for line in content:
+#          dedi.append(line.split(':'))
+#
+#
+# print(dedi)
+#second way
+# with open('devices.txt') as f:
+#     rader = csv.reader(f, delimiter=':')
+#     mylist=list()
+#     for row in rader:
+#         mylist.append(row)
+# print(mylist)
+
+# with open('macs.txt') as f:
+#     content = f.read().split()
+#     print(content)
+#     content = list(set(content))
+#     print(content)
+#
+# with open('mac_uniqe.txt','w', newline='') as f:
+#     for mac in content:
+#         f.write(f'{mac}\n')
+
+# with open('sample_file.txt') as f:
+#     content=f.read().splitlines()
+#     print(content)
+#     my_str= '\n'.join(content)
+#     print(my_str)
+# with open('file.txt','r', newline='') as f:
+#      context=f.readlines()
+#      print(context)
+#      tmp_list=list()
+#      for line in context:
+#          if line.strip() != '':
+#              tmp_list.append(line)
+# print(tmp_list)
+#
+# with open('fileWithout.txt', 'w') as f:
+#     ali = f.write(''.join(tmp_list))
+# import time
+# def tail(txt,n):
+#     with open(txt,'r') as f:
+#         content = f.read().splitlines()
+#         last = content[len(content)-n:]
+#         print(last)
+#         my_str='\n'.join(last)
+#         return my_str
+#
+# while True:
+#     t=tail('sample_file.txt',3)
+#     print(t)
+#     time.sleep(3)
+#     print('')
+# def wc(txt):
+#     with open(txt,'r') as f:
+#         content=f.read().splitlines()
+#         print(content)
+#         lines = len(content)
+#         print(list(content[1]))
+#         words=0
+#         for line in content:
+#             words += len(line.split())
+#
+#         chars=0
+#         for line in content:
+#             chars += len(list(line))
+#
+#
+#         return  chars, lines, words
+#
+#
+# print(wc('sample_file.txt'))
+# with open('banking.txt', 'r') as f:
+#     content = f.read().splitlines()
+#     print(content)
+#     deposit, withdrawal = 0,0
+#
+#     for item in content:
+#       tmp=item.split(':')
+#       print(tmp)
+#       if tmp[0] == 'D':
+#             deposit += int(tmp[1])
+#       elif tmp[0] == 'W':
+#           withdrawal +=int(tmp[1])
+#       else:
+#           print('File Format eror')
+#
+#     blance = deposit-withdrawal
+#     print(blance)
+# with open('american-english.txt', 'r') as f:
+#     words= f.read().splitlines()
+#     print(words)
+#
+#     words_and_length = dict()
+#     for w in words:
+#         words_and_length[w] = len(w)
+#         print(w)
+#     for k,v in words_and_length.items():
