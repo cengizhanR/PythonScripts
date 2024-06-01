3c0669b836876ffbf75e31e29c386bb469c7df5d
cengi <cengizjan1457@gmail..com>
2024-06-01 18:21:37 +0300

    new

--- paramikoconnect.py
+++ paramikoconnect.py
@@ -41,38 +41,89 @@ import time
 #     print('Closing connection')
 #     ssh_client.close()
 
+# ssh_client = paramiko.SSHClient()
+# ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#
+# # creating a dictionary for each device to connect to
+# router1 = {'hostname': '192.168.122.10', 'port': '22', 'username': 'u1', 'password': 'cisco'}
+# router2 = {'hostname': '192.168.122.10', 'port': '22', 'username': 'u1', 'password': 'cisco'}
+# router3 = {'hostname': '192.168.122.10', 'port': '22', 'username': 'u1', 'password': 'cisco'}
+#
+# # creating a list of dictionaries (of devices)
+# routers = [router1, router2, router3]
+#
+# # iterating over the list (over the devices) and backup the config
+# for router in routers:
+#     print(f'Connecting to {router["hostname"]}')
+#     ssh_client.connect(**router, look_for_keys=False, allow_agent=False)
+#
+#     shell = ssh_client.invoke_shell()
+#
+#     shell.send('enable\n')
+#     shell.send('cisco\n')  # this is the enable password
+#     shell.send('conf t\n')
+#     shell.send('router ospf 1\n')
+#     shell.send('net 0.0.0.0 0.0.0.0 area 0\n')
+#     shell.send('end\n')
+#     shell.send('terminal length 0\n')
+#     shell.send('sh ip protocols\n')
+#     time.sleep(2)
+#
+#     output = shell.recv(10000).decode()
+#     print(output)
+#
+#     if ssh_client.get_transport().is_active() == True:
+#         print('Closing connection...')
+#         ssh_client.close()
+
+# ssh_client = paramiko.SSHClient()
+# ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#
+# # creating a dictionary for each device to connect to
+# linux = {'hostname': '192.168.56.2', 'port': '22', 'username': 'ubuntunode01', 'password': '123'}
+#
+# print(f'Connecting to {linux["hostname"]}')
+# ssh_client.connect(**linux, look_for_keys=False, allow_agent=False)
+#
+# stdin,stdout,stderr= ssh_client.exec_command('ifconfig\n')
+# output=stdout.read()
+# output=output.decode()
+# print(output)
+#
+# stdin,stdout,stderr= ssh_client.exec_command('who\n')
+# time.sleep(0.5)
+# output=stdout.read()
+# output=output.decode()
+# print(output)
+#
+# stdin,stdout,stderr= ssh_client.exec_command('cat /etc/shadow\n')
+# time.sleep(0.5)
+# output=stdout.read()
+# output=output.decode()
+# print(output)
+# print(stderr.read().decode())
+# if ssh_client.get_transport().is_active() == True:
+#     print('Closing connection...')
+#     ssh_client.close()
+
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 # creating a dictionary for each device to connect to
-router1 = {'hostname': '192.168.122.10', 'port': '22', 'username': 'u1', 'password': 'cisco'}
-router2 = {'hostname': '192.168.122.10', 'port': '22', 'username': 'u1', 'password': 'cisco'}
-router3 = {'hostname': '192.168.122.10', 'port': '22', 'username': 'u1', 'password': 'cisco'}
+linux = {'hostname': '192.168.56.2', 'port': '22', 'username': 'ubuntunode01', 'password': '123'}
 
-# creating a list of dictionaries (of devices)
-routers = [router1, router2, router3]
+print(f'Connecting to {linux["hostname"]}')
+ssh_client.connect(**linux, look_for_keys=False, allow_agent=False)
 
-# iterating over the list (over the devices) and backup the config
-for router in routers:
-    print(f'Connecting to {router["hostname"]}')
-    ssh_client.connect(**router, look_for_keys=False, allow_agent=False)
+stdin, stdout, stderr = ssh_client.exec_command('sudo useradd u3\n', get_pty=True)
 
-    shell = ssh_client.invoke_shell()
+stdin.write('123\n')
+time.sleep(2)
+print(stderr.read().decode())
+stdin, stdout, stderr = ssh_client.exec_command('cat /etc/passwd\n')
+print(stdout.read().decode())
+time.sleep(1)
 
-    shell.send('enable\n')
-    shell.send('cisco\n')  # this is the enable password
-    shell.send('conf t\n')
-    shell.send('router ospf 1\n')
-    shell.send('net 0.0.0.0 0.0.0.0 area 0\n')
-    shell.send('end\n')
-    shell.send('terminal length 0\n')
-    shell.send('sh ip protocols\n')
-    time.sleep(2)
-
-    output = shell.recv(10000).decode()
-    print(output)
-
-    if ssh_client.get_transport().is_active() == True:
-        print('Closing connection...')
-        ssh_client.close()
-
+if ssh_client.get_transport().is_active() == True:
+    print('Closing connection...')
+    ssh_client.close()
